384d80bb620a6d9db711241bef36fa15427a6841
fanhua1994 <18383219470@163.com>
2018-01-24 10:37:01 +0800

    更新demo

--- src/main/java/com/hengyi/controller/MainController.java
+++ src/main/java/com/hengyi/controller/MainController.java
@@ -2,6 +2,10 @@ package com.hengyi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.hengyi.dzfilter.utils.TextUtils;
 
 @Controller
 public class MainController {
@@ -10,5 +14,18 @@ public class MainController {
     public String IndexPage() {
         return "index";
     }
-
+    
+    //过滤消息
+    @RequestMapping(value="/filter",produces="text/html;charset=utf-8")
+    @ResponseBody
+    public String FilterPage(@RequestParam String content) {
+        return TextUtils.filter(content);
+    }
+    
+    
+    @RequestMapping(value="/delfilter",produces="text/html;charset=utf-8")
+    @ResponseBody
+    public String DelFilterPage(@RequestParam String content) {
+        return "删除结果：" + TextUtils.delFilter(content);
+    }
 }
